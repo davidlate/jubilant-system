2604b472099a8f6d641dd1a8ca6db7d875796ae0
David Earley <david.earley2@gmail.com>
2022-11-27 08:32:55 -0500

    Added menu page and basic styling

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ window.addEventListener('scroll', () =>{
     let header = document.querySelector('.header-container');
     let upper = document.querySelectorAll('.upper')
     let pos = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+    let headerHeight = window.getComputedStyle(document.querySelector('.header')).getPropertyValue('height');
+    let menuPos = document.querySelector('.menu-page');
+    
     let newHeight = 3*16 - pos/10;
     let newScale = 1-pos*.001;
 
@@ -10,9 +13,14 @@ window.addEventListener('scroll', () =>{
     if (newHeight < 30) newHeight = 30;
     if (newScale < 0.8) newScale = 0.8;
 
+    
+    menuPos.style.top = headerHeight;
+
     header.style.height= `${newHeight}px`;
     upper.forEach(div => {
         div.style.scale = `${newScale} `;
     })
-    console.log(newHeight);
+
+
+    console.log(headerHeight);
 })
